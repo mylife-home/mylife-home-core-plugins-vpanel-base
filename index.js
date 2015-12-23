84de7d7d862e19497315c53d8c1f8e1688925903
Vincent Trumpff <vincent@stoic.com>
2015-12-23 14:44:36 +0100

    added conversions

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = {
-  StepRelay : require('./lib/step-relay'),
-  Timer     : require('./lib/timer')
+  StepRelay           : require('./lib/step-relay'),
+  Timer               : require('./lib/timer'),
+
+  // converters
+  BinaryToPercent     : require('./lib/binary-to-percent'),
+  PercentToBinary     : require('./lib/percent-to-binary')
 };
