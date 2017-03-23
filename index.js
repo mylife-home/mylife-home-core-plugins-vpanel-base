c7797e5f679f0ceca3b56a1b9dc2a07170529996
Vincent Trumpff <vincent@stoic.com>
2017-03-23 17:54:58 +0100

    added value*

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 'use strict';
 
 module.exports = {
-  StepRelay           : require('./lib/step-relay'),
-  Timer               : require('./lib/timer'),
+  StepRelay            : require('./lib/step-relay'),
+  Timer                : require('./lib/timer'),
 
   // converters
-  BinaryToPercent     : require('./lib/binary-to-percent'),
-  PercentToBinary     : require('./lib/percent-to-binary'),
-  SwitchToButton      : require('./lib/switch-to-button'),
-  BinaryToPulse       : require('./lib/binary-to-pulse'),
+  BinaryToPercent      : require('./lib/binary-to-percent'),
+  PercentToBinary      : require('./lib/percent-to-binary'),
+  SwitchToButton       : require('./lib/switch-to-button'),
+  BinaryToPulse        : require('./lib/binary-to-pulse'),
 
   // constants
-  ConstantBinary      : require('./lib/constant-binary'),
-  Constant_0_100      : require('./lib/constant-0-100'),
-  Constant_0_255      : require('./lib/constant-0-255'),
+  ConstantBinary       : require('./lib/constant-binary'),
+  Constant_0_100       : require('./lib/constant-0-100'),
+  Constant_0_255       : require('./lib/constant-0-255'),
+
+  // value
+  ValueBinary          : require('./lib/value-binary'),
+  ValueNullablePercent : require('./lib/value-nullable-percent'),
 };
